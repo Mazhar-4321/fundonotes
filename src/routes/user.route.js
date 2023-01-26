fa6f42c7f2f7b01c47ec48ed0f58febdcca77b42
Mazhar-4321 <syedmazharali742@gmail.com>
2023-01-26 08:57:20 +0530

    ADDED: MiddleWare To Login URL For Validation

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import * as userController from '../controllers/user.controller';
 import { newUserValidator } from '../validators/user.validator';
-import { userAuth } from '../middlewares/auth.middleware';
+import { LoginValidator } from '../validators/user.validator';
 
 const router = express.Router();
 
 //route to get all users
 
 router.post('/registration', newUserValidator, userController.registerUser);
-router.post('/login', userController.signInUser);
+router.post('/login',LoginValidator, userController.signInUser);
 
 
 
