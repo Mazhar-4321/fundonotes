0b7e9ee779a186a5cddf1001cc00c8f462d58c6d
Mazhar-4321 <syedmazharali742@gmail.com>
2023-01-26 07:28:37 +0530

    ADDED: User Validator To Validate Data Using JOI

--- src/validators/user.validator.js
+++ src/validators/user.validator.js
@@ -1,8 +1,19 @@
-import Joi from '@hapi/joi';
-
+const Joi = require('joi')
+const { joiPasswordExtendCore } = require('joi-password');
+const joiPassword = Joi.extend(joiPasswordExtendCore);
 export const newUserValidator = (req, res, next) => {
   const schema = Joi.object({
-    name: Joi.string().min(4).required()
+    firstName: Joi.string().pattern(new RegExp('([A-Z][a-zA-Z]{2,})')).required(),
+    lastName: Joi.string().pattern(new RegExp('([A-Z][a-zA-Z]{2,})')).required(),
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+    password: joiPassword
+    .string()
+    .minOfSpecialCharacters(1)
+    .minOfLowercase(4)
+    .minOfUppercase(1)
+    .minOfNumeric(2)
+    .noWhiteSpaces()
+    .required(),
   });
   const { error, value } = schema.validate(req.body);
   if (error) {
