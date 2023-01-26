685be276a90db35c8d9905396d32604b4fd4044f
Mazhar-4321 <syedmazharali742@gmail.com>
2023-01-26 20:09:34 +0530

    REMOVED: WhiteSpaces And Comments

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,11 +1,5 @@
 import User from '../models/user.model';
 const bcrypt = require('bcrypt');
-//get all users
-export const getAllUsers = async () => {
-  const data = await User.find();
-  return data;
-};
-// Add User To Document
 export const registerUser = async (userData) => {
   try {
     userData.password = bcrypt.hashSync(userData.password, 10);
@@ -15,17 +9,16 @@ export const registerUser = async (userData) => {
     throw new Error(err)
   }
 }
-// Retrieve Data Of User
 export const signIn = async (userData) => {
   try {
     const data = await User.find({
       "email": userData.email
     })
     console.log(data)
-    if (data.length>=1) {
-    console.log("data successful",data[0].password,userData.password)
-      const verified = bcrypt.compareSync( userData.password,data[0].password)
-      console.log("verified",verified)
+    if (data.length >= 1) {
+      console.log("data successful", data[0].password, userData.password)
+      const verified = bcrypt.compareSync(userData.password, data[0].password)
+      console.log("verified", verified)
       if (!verified) {
         throw new Error('Invalid Password')
       } else {
