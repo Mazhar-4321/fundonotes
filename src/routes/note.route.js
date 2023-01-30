a9072a1e8743275c7576639df7e749120c6dc0b6
Mazhar-4321 <syedmazharali742@gmail.com>
2023-01-30 18:59:20 +0530

    MODIFIED: Note Route To Replace Title By Id

--- src/routes/note.route.js
+++ src/routes/note.route.js
@@ -2,10 +2,17 @@ import express from 'express'
 import * as NoteController from '../controllers/note.controller'
 import { newNoteValidator } from '../validators/note.validator'
 import { userAuth } from '../middlewares/auth.middleware'
+
 const router = express.Router()
+
 router.post('/', newNoteValidator,userAuth, NoteController.createNote)
-router.put('/:title',userAuth, NoteController.updateNote)
+
+router.put('/:id',userAuth, NoteController.updateNote)
+
 router.get('/',userAuth,NoteController.findAllNotes)
-router.get('/:title',userAuth,NoteController.findNote)
-router.delete('/:title',userAuth,NoteController.deleteNote)
+
+router.get('/:id',userAuth,NoteController.findNote)
+
+router.delete('/:id',userAuth,NoteController.deleteNote)
+
 export default router
