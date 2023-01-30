e5126898884d24a0b551909aa24a166bfd6c2779
Mazhar-4321 <syedmazharali742@gmail.com>
2023-01-30 14:18:25 +0530

    MODIFIED: UpdateNote Route To Http PUT Method

--- src/routes/note.route.js
+++ src/routes/note.route.js
@@ -4,7 +4,7 @@ import { newNoteValidator } from '../validators/note.validator'
 import { userAuth } from '../middlewares/auth.middleware'
 const router = express.Router()
 router.post('/', newNoteValidator,userAuth, NoteController.createNote)
-router.post('/:title',userAuth, NoteController.updateNote)
+router.put('/:title',userAuth, NoteController.updateNote)
 router.get('/',userAuth,NoteController.findAllNotes)
 router.get('/:title',userAuth,NoteController.findNote)
 router.delete('/:title',userAuth,NoteController.deleteNote)
