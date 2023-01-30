6ab632700d788ef489872bdcfa02cd6cc16f400a
Mazhar-4321 <syedmazharali742@gmail.com>
2023-01-30 09:54:54 +0530

    ADDED: Note Model To The Project

---
+++ src/models/note.model.js
@@ -0,0 +1,28 @@
+import { Schema, model } from 'mongoose';
+const noteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      unique:true
+    },
+    description:{
+      type:String
+    },
+    color:{
+      type:String,
+    },
+    archive:{
+      type:Boolean
+    },
+    trash:{
+        type:Boolean
+    },
+    userId:{
+        type:String
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+export default model('Note', noteSchema);
