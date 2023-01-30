d130da2fe77e2a31f82ec72b7a4fe5c900ae9cf7
Mazhar-4321 <syedmazharali742@gmail.com>
2023-01-30 14:38:04 +0530

    MODIFIED: Title And Description Validation Criteria

--- src/validators/note.validator.js
+++ src/validators/note.validator.js
@@ -1,8 +1,8 @@
 const Joi = require('joi')
 export const newNoteValidator = (req, res, next) => {
     const schema = Joi.object({
-        title: Joi.string().pattern(new RegExp('([A-Z][a-zA-Z]{2,})')).required(),
-        description: Joi.string().pattern(new RegExp('([A-Z][a-zA-Z]{9,})')).required(),
+        title: Joi.string().min(3).required(),
+        description: Joi.string().min(3).required(),
         color: Joi.string().min(3).optional(),
         archive: Joi.boolean().optional(),
         trash: Joi.boolean().optional(),
