1cc7f00339add49799e655da1a5d05504368a7b4
Mazhar-4321 <syedmazharali742@gmail.com>
2023-01-30 10:45:18 +0530

    ADDED: NotesRoute To Project Path

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import userRoute from './user.route';
+import notesRoute from './note.route'
 const router = express.Router();
 const routes = () => {
   router.get('/', (req, res) => {
     res.json('Welcome');
   });
   router.use('/users', userRoute);
-
+  router.use('/notes', notesRoute);
   return router;
 };
 export default routes;
