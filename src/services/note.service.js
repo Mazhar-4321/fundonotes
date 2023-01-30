5d80a24271d396db00ca90e265bc1ba114528ebe
Mazhar-4321 <syedmazharali742@gmail.com>
2023-01-30 18:57:38 +0530

    MODIFIED: User Service To Rename Parameters

--- src/services/note.service.js
+++ src/services/note.service.js
@@ -1,4 +1,5 @@
 import Note from '../models/note.model'
+
 export const createNote = async (note) => {
   try {
     const data = await Note.create(note)
@@ -7,22 +8,25 @@ export const createNote = async (note) => {
     throw new Error(err)
   }
 }
-export const deleteNote = async (title) => {
+
+export const deleteNote = async (id) => {
   try {
-    const data = await Note.findOneAndDelete({ "title": title })
+    const data = await Note.findOneAndDelete({ "_id": id })
     return data
   } catch (err) {
     throw new Error(err)
   }
 }
-export const findNote = async (title) => {
+
+export const findNote = async (id) => {
   try {
-    const data = await Note.findOne({ "title": title })
+    const data = await Note.findOne({ "_id": id })
     return data
   } catch (err) {
     throw new Error(err)
   }
 }
+
 export const findAllNotes = async () => {
   try {
     const data = await Note.find()
@@ -31,9 +35,10 @@ export const findAllNotes = async () => {
     throw new Error(err)
   }
 }
-export const updateNote = async (title, description) => {
+
+export const updateNote = async (id, description) => {
   try {
-    const filter = { "title": title }
+    const filter = { "_id": id }
     const update = { "description": description }
     const data = await Note.findOneAndUpdate(filter, update)
     return data
