619fea7785adf5201503e1adb40c44fce088aaa0
Mazhar-4321 <syedmazharali742@gmail.com>
2023-01-30 14:31:59 +0530

    FORMATTED: Documents Of The Project

--- src/controllers/note.controller.js
+++ src/controllers/note.controller.js
@@ -1,62 +1,62 @@
-import HttpStatus from 'http-status-codes';
-import * as NoteService from '../services/note.service';
+import HttpStatus from 'http-status-codes'
+import * as NoteService from '../services/note.service'
 export const createNote = async (req, res, next) => {
   try {
-    const data = await NoteService.createNote(req.body);
+    const data = await NoteService.createNote(req.body)
     res.status(HttpStatus.OK).json({
       code: HttpStatus.OK,
-      data:data,
+      data: data,
       message: 'Note Added Successfully'
     });
   } catch (error) {
     next(error);
   }
 };
-export const updateNote=async(req,res,next)=>{
-    try {
-        const data = await NoteService.updateNote(req.params.title,req.body.description);
-        res.status(HttpStatus.OK).json({
-          code: HttpStatus.OK,
-          data:data,
-          message: 'Note Updated Successfully'
-        });
-      } catch (error) {
-        next(error);
-      }
+export const updateNote = async (req, res, next) => {
+  try {
+    const data = await NoteService.updateNote(req.params.title, req.body.description);
+    res.status(HttpStatus.OK).json({
+      code: HttpStatus.OK,
+      data: data,
+      message: 'Note Updated Successfully'
+    });
+  } catch (error) {
+    next(error);
+  }
 }
-export const deleteNote=async(req,res,next)=>{
-    try {
-        const data = await NoteService.deleteNote(req.params.title);
-        res.status(HttpStatus.OK).json({
-          code: HttpStatus.OK,
-          data:data,
-          message: 'Note Deleted Successfully'
-        });
-      } catch (error) {
-        next(error);
-      }
+export const deleteNote = async (req, res, next) => {
+  try {
+    const data = await NoteService.deleteNote(req.params.title);
+    res.status(HttpStatus.OK).json({
+      code: HttpStatus.OK,
+      data: data,
+      message: 'Note Deleted Successfully'
+    });
+  } catch (error) {
+    next(error)
+  }
 }
-export const findNote=async(req,res,next)=>{
-    try {
-        const data = await NoteService.findNote(req.params.title);
-        res.status(HttpStatus.OK).json({
-          code: HttpStatus.OK,
-          data:data,
-        });
-      } catch (error) {
-        next(error);
-      }
+export const findNote = async (req, res, next) => {
+  try {
+    const data = await NoteService.findNote(req.params.title);
+    res.status(HttpStatus.OK).json({
+      code: HttpStatus.OK,
+      data: data,
+    });
+  } catch (error) {
+    next(error)
+  }
 }
-export const findAllNotes=async(req,res,next)=>{
-    try {
-        const data = await NoteService.findAllNotes();
-        res.status(HttpStatus.OK).json({
-          code: HttpStatus.OK,
-          data:data,
-        });
-      } catch (error) {
-        next(error);
-      }
+export const findAllNotes = async (req, res, next) => {
+  try {
+    const data = await NoteService.findAllNotes();
+    res.status(HttpStatus.OK).json({
+      code: HttpStatus.OK,
+      data: data,
+    });
+  } catch (error) {
+    next(error)
+  }
 }
 
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import userRoute from './user.route';
+import express from 'express'
+import userRoute from './user.route'
 import notesRoute from './note.route'
-const router = express.Router();
+const router = express.Router()
 const routes = () => {
   router.get('/', (req, res) => {
     res.json('Welcome');
   });
-  router.use('/users', userRoute);
-  router.use('/notes', notesRoute);
-  return router;
+  router.use('/users', userRoute)
+  router.use('/notes', notesRoute)
+  return router
 };
-export default routes;
+export default routes
