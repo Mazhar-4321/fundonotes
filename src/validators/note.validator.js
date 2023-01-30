97bcfa87f52cc4d4f52f55768fbb23a468a0ccb6
Mazhar-4321 <syedmazharali742@gmail.com>
2023-01-30 18:58:38 +0530

    REMOVED: Optional Fields From Note Validator

--- src/validators/note.validator.js
+++ src/validators/note.validator.js
@@ -1,18 +1,16 @@
 const Joi = require('joi')
+
 export const newNoteValidator = (req, res, next) => {
     const schema = Joi.object({
         title: Joi.string().min(3).required(),
         description: Joi.string().min(3).required(),
         color: Joi.string().min(3).optional(),
-        archive: Joi.boolean().optional(),
-        trash: Joi.boolean().optional(),
-        userId: Joi.string().min(3).optional()
     });
     const { error, value } = schema.validate(req.body);
     if (error) {
+        console.log(error)
         next(error);
     } else {
-        req.validatedBody = value;
         next();
     }
-};
+}
