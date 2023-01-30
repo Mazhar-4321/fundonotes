d98529b3f47cdf48a5e7432548adab03996dd470
Mazhar-4321 <syedmazharali742@gmail.com>
2023-01-30 10:47:20 +0530

    MODIFIED: Authentication Middleware For UserAuth

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,14 +1,5 @@
 import HttpStatus from 'http-status-codes';
 import jwt from 'jsonwebtoken';
-
-/**
- * Middleware to authenticate if user has a valid Authorization token
- * Authorization: Bearer <token>
- *
- * @param {Object} req
- * @param {Object} res
- * @param {Function} next
- */
 export const userAuth = async (req, res, next) => {
   try {
     let bearerToken = req.header('Authorization');
@@ -18,10 +9,7 @@ export const userAuth = async (req, res, next) => {
         message: 'Authorization token is required'
       };
     bearerToken = bearerToken.split(' ')[1];
-
-    const { user } = await jwt.verify(bearerToken, 'your-secret-key');
-    res.locals.user = user;
-    res.locals.token = bearerToken;
+    const user = await jwt.verify(bearerToken, process.env.SECRET_KEY);
     next();
   } catch (error) {
     next(error);
