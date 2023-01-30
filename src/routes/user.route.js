11ad700ea74db5b882a8a7d6ee354a149354503d
Mazhar-4321 <syedmazharali742@gmail.com>
2023-01-30 07:28:08 +0530

    MODIFIED: Registration URL To Universal URL

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,6 +3,6 @@ import * as userController from '../controllers/user.controller';
 import { newUserValidator } from '../validators/user.validator';
 import { LoginValidator } from '../validators/user.validator';
 const router = express.Router();
-router.post('/registration', newUserValidator, userController.registerUser);
-router.post('/login', LoginValidator, userController.signInUser);
+router.post('/', newUserValidator, userController.registerUser);
+router.post('/login', userController.signInUser);
 export default router;
