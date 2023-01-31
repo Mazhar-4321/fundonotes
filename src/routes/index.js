d24debe8acf485690327e6e2333b72d8842e5713
Mazhar-4321 <syedmazharali742@gmail.com>
2023-01-31 19:36:16 +0530

    [Mazhar]ADDED: Notes/Trash URL To Index Route Of  Project

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import userRoute from './user.route'
 import notesRoute from './note.route'
+import notesTrashRoute from './note.trash.route'
 
 const router = express.Router()
 
@@ -9,6 +10,7 @@ const routes = () => {
     res.json('Welcome');
   });
   router.use('/users', userRoute)
+  router.use('/notes/trash',notesTrashRoute)
   router.use('/notes', notesRoute)
   return router
 }
