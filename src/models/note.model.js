c0354fd445ac71c55202d27e2c6afce45d7a3391
Mazhar-4321 <syedmazharali742@gmail.com>
2023-01-31 15:02:00 +0530

    [Mazhar]MODIFIED: Note Model To Make Default Archive And Trash Values False

--- src/models/note.model.js
+++ src/models/note.model.js
@@ -11,10 +11,12 @@ const noteSchema = new Schema(
       type: String,
     },
     archive: {
-      type: Boolean
+      type: Boolean,
+      default:false
     },
     trash: {
-      type: Boolean
+      type: Boolean,
+      default:false
     },
     userId: {
       type: String
