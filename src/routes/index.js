67b40640e270e25f39cb5124a16dc2158c56fd9e
Mazhar-4321 <syedmazharali742@gmail.com>
2023-02-01 04:35:35 +0530

    [Mazhar]ADDED: NotesArchiveRoute  To Project Routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import userRoute from './user.route'
 import notesRoute from './note.route'
 import notesTrashRoute from './note.trash.route'
+import notesArchiveRoute from './note.archive.route'
 
 const router = express.Router()
 
@@ -11,6 +12,7 @@ const routes = () => {
   });
   router.use('/users', userRoute)
   router.use('/notes/trash',notesTrashRoute)
+  router.use('/notes/archive',notesArchiveRoute)
   router.use('/notes', notesRoute)
   return router
 }
