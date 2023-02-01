68083d650f613093309d1e89173385058bacd24e
Mazhar-4321 <syedmazharali742@gmail.com>
2023-02-01 17:17:14 +0530

    [Mazhar]ADDED: Trash And Archive Routes To Note Route

--- src/routes/note.route.js
+++ src/routes/note.route.js
@@ -7,7 +7,21 @@ const router = express.Router()
 
 router.post('/', newNoteValidator,userAuth, NoteController.createNote)
 
+router.put('/email',NoteController.sendEmail)
+
+router.put('trash/:id',userAuth,NoteController.updateTrashNote)
+
+router.put('archive/:id',userAuth,NoteController.updateNoteArchiveStatus)
+
 router.put('/:id',newNoteValidator,userAuth, NoteController.updateNote)
+
+router.get('/trash/:id',userAuth,NoteController.findTrashNote)
+
+router.get('/archive/:id',userAuth,NoteController.findArchivedNote)
+
+router.get('/trash',userAuth,NoteController.findAllTrashNotes)
+
+router.get('/archive',userAuth,NoteController.findAllArchivedNotes)
 
 router.get('/:id',userAuth,NoteController.findNote)
 
@@ -15,4 +29,6 @@ router.get('/',userAuth,NoteController.findAllNotes)
 
 router.delete('/:id',userAuth,NoteController.deleteNote)
 
+router.delete('trash/:id',userAuth,NoteController.deleteTrashNote)
+ 
 export default router
