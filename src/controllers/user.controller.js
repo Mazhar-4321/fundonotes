a0faa9933fc4b635b96781faf8309765f55d7350
Mazhar-4321 <syedmazharali742@gmail.com>
2023-02-02 15:54:44 +0530

    [Mazhar]ADDED: Http Responses On All Test Cases

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,50 +3,69 @@ import * as UserService from '../services/user.service';
 export const registerUser = async (req, res, next) => {
   try {
     const data = await UserService.registerUser(req.body);
+    if(data){
     res.status(HttpStatus.OK).json({
       code: HttpStatus.OK,
       message: 'User Registered Successfully'
     });
+  }else{
+    throw new Error('Database Operation Failed')
+  }
   } catch (error) {
     next(error);
   }
 };
-export const signInUser=async(req,res,next)=>{
+export const signInUser = async (req, res, next) => {
   try {
     const data = await UserService.signIn(req.body);
+    if(data){
     res.status(HttpStatus.OK).json({
       code: HttpStatus.OK,
       data: req.body.email,
-      token:data
+      token: data
     });
+  }else{
+    throw new Error('Database Operation Failed')
+  }
   } catch (error) {
-    next(error);
+    res.status(HttpStatus.NOT_FOUND).json({
+      code: HttpStatus.NOT_FOUND,
+      error: error.message.split(":")[1],
+    });
   }
 }
 
-export const forgetPassword=async(req,res,next)=>{
+export const forgetPassword = async (req, res, next) => {
   try {
     const data = await UserService.forgotPassword(req.body);
     res.status(HttpStatus.OK).json({
       code: HttpStatus.OK,
       data: req.body.email,
-      token:data
+      token: data
     });
   } catch (error) {
-    next(error);
+    res.status(HttpStatus.NOT_FOUND).json({
+      code: HttpStatus.NOT_FOUND,
+      error: error.message.split(":")[1],
+    });
   }
 }
 
-export const resetPassword=async(req,res,next)=>{
+export const resetPassword = async (req, res, next) => {
   try {
-    console.log("controller")
     const data = await UserService.resetPassword(req);
-    res.status(HttpStatus.OK).json({
-      code: HttpStatus.OK,
-      data: req.body.email,
-      token:data
-    });
+    if (data) {
+      res.status(HttpStatus.OK).json({
+        code: HttpStatus.OK,
+        msg: "Password Reset Successful"
+      });
+    } else {
+      throw new Error('Database Operation Failed')
+    }
   } catch (error) {
-    next(error);
+    res.status(HttpStatus.BAD_REQUEST).json({
+      code: HttpStatus.BAD_REQUEST,
+      error: error.message.split(":")[1],
+    });
   }
 }
