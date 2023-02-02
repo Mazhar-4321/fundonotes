9cbadcadb99dd53740da197800cdf12787027bf1
Mazhar-4321 <syedmazharali742@gmail.com>
2023-02-02 15:31:30 +0530

    [Mazhar]REMOVED: Send Email Logic From Note Controller

--- src/controllers/note.controller.js
+++ src/controllers/note.controller.js
@@ -1,9 +1,9 @@
-import HttpStatus from 'http-status-codes'
-import * as NoteService from '../services/note.service'
+import HttpStatus from 'http-status-codes';
+import * as NoteService from '../services/note.service';
 
 export const createNote = async (req, res, next) => {
   try {
-    const data = await NoteService.createNote(req.body)
+    const data = await NoteService.createNote(req.body);
     res.status(HttpStatus.OK).json({
       code: HttpStatus.OK,
       data: data,
@@ -12,7 +12,7 @@ export const createNote = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-}
+};
 
 export const updateNote = async (req, res, next) => {
   try {
@@ -25,7 +25,7 @@ export const updateNote = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-}
+};
 
 export const deleteNote = async (req, res, next) => {
   try {
@@ -36,57 +36,56 @@ export const deleteNote = async (req, res, next) => {
       message: 'Note Moved To Trash Successfully'
     });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 export const findNote = async (req, res, next) => {
   try {
     const data = await NoteService.findNote(req);
     res.status(HttpStatus.OK).json({
       code: HttpStatus.OK,
-      data: data,
+      data: data
     });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
-
+};
 export const findAllNotes = async (req, res, next) => {
   try {
     const data = await NoteService.findAllNotes(req.body.userId);
     res.status(HttpStatus.OK).json({
       code: HttpStatus.OK,
-      data: data,
+      data: data
     });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 export const findAllTrashNotes = async (req, res, next) => {
   try {
     const data = await NoteService.findAllTrashNotes(req.body.userId);
     res.status(HttpStatus.OK).json({
       code: HttpStatus.OK,
-      data: data,
+      data: data
     });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 export const findTrashNote = async (req, res, next) => {
   try {
     const data = await NoteService.findTrashNote(req);
     res.status(HttpStatus.OK).json({
       code: HttpStatus.OK,
-      data: data,
+      data: data
     });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 export const updateTrashNote = async (req, res, next) => {
   try {
@@ -99,7 +98,7 @@ export const updateTrashNote = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-}
+};
 
 export const deleteTrashNote = async (req, res, next) => {
   try {
@@ -112,31 +111,31 @@ export const deleteTrashNote = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-}
+};
 
 export const findAllArchivedNotes = async (req, res, next) => {
   try {
     const data = await NoteService.findAllArchivedNotes(req.body.userId);
     res.status(HttpStatus.OK).json({
       code: HttpStatus.OK,
-      data: data,
+      data: data
     });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 export const findArchivedNote = async (req, res, next) => {
   try {
     const data = await NoteService.findArchivedNote(req);
     res.status(HttpStatus.OK).json({
       code: HttpStatus.OK,
-      data: data,
+      data: data
     });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 export const updateNoteArchiveStatus = async (req, res, next) => {
   try {
@@ -149,6 +148,6 @@ export const updateNoteArchiveStatus = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-}
+};
 
 
