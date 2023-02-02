9398a963ad99cedb16488997c0e82e828cd4d86a
Mazhar-4321 <syedmazharali742@gmail.com>
2023-02-02 15:23:39 +0530

    [Mazhar]ADDED: Reset Password Function To User Service

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,8 +1,10 @@
 import User from '../models/user.model';
-import Token from '../models/token.model;'
+import Token from '../models/token.model';
 import Jwt from 'jsonwebtoken';
 import { sendEmail } from '../utils/user.util';
+
 const bcrypt = require('bcrypt');
+
 export const registerUser = async (userData) => {
   try {
     userData.password = bcrypt.hashSync(userData.password, 10);
@@ -12,6 +14,7 @@ export const registerUser = async (userData) => {
     throw new Error(err)
   }
 }
+
 export const signIn = async (userData) => {
   try {
     const data = await User.find({
@@ -33,24 +36,19 @@ export const signIn = async (userData) => {
     throw new Error(err)
   }
 }
-const getRandomString = () => {
-  let result = ' ';
-  const characters = 'QWERTYUIOPASDFGHJKLZXCVBNM'
-  const charactersLength = characters.length;
-  let counter = 0
-  while (counter < charactersLength) {
-    result += characters[Math.floor(Math.random() * charactersLength)]
-    counter++
-  }
-  return result
-}
+
 export const forgotPassword = async (email) => {
   try {
-    const data = User.findOne({ email: email })
+    let data = User.findOne({ email: email.email })
     if (data) {
-      const randomString = getRandomString()
-      const link=`${process.env.APP_HOST}:${process.env.APP_PORT}/api/${process.env.APP_VERSION}/users/reset/${randomString}`;
-      sendEmail(email,link)
+      var token = Jwt.sign({ email: email.email }, process.env.FORGET_PASSWORD_SECRET_KEY);
+      data = await Token.create({
+        token: token,
+        expiry: 1200000,
+        createdTime: new Date().getTime()
+      })
+      const link = `${process.env.APP_HOST}:${process.env.APP_PORT}/api/${process.env.API_VERSION}/users/reset/${data.token}`;
+      sendEmail(email.email, link)
     } else {
       throw new Error('Email Doesn\'t Exist')
     }
@@ -60,4 +58,31 @@ export const forgotPassword = async (email) => {
   }
 }
 
+export const resetPassword = async (req) => {
+  try {
+    let data = await Token.findOne({ token: req.params.id })
+    if (data) {
+      var currentTime = new Date().getTime();
+      var tokenTime = data.createdTime;
+      if (currentTime - tokenTime > data.expiry) {
+        throw new Error('Token Expired')
+      }
+      let userDetails = await User.findOne({ email: req.body.userId })
+      if (userDetails) {
+        const newPassword = bcrypt.hashSync(req.body.password, 10);
+        const filter = { email: req.body.userId };
+        const update = { password: newPassword };
+        const data = await User.findOneAndUpdate(filter, update);
+      } else {
+        throw new Error('User Doesn\'t Exist')
+      }
+
+    } else {
+      throw new Error('Token Doesn\'t Exist')
+    }
+  }
+  catch (err) {
+    throw new Error(err)
+  }
+}
 
