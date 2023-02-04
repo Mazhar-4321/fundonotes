2a0f5a9ab9dc81d4dfd948aa193fac05110ddf14
Mazhar-4321 <syedmazharali742@gmail.com>
2023-02-04 07:55:18 +0530

    [Mazhar]ADDED: Custom Logger Format

---
+++ src/config/myLogger.js
@@ -0,0 +1,24 @@
+import winston from 'winston';
+import { createLogger, format, transports } from 'winston'
+
+const { combine, timestamp, label, printf } = format
+const myFormat = printf(({ level, message, label, timestamp }) => {
+    return `[${timestamp}]  ${level}: ${message}`;
+  });
+  /***
+   * winston.format.timestamp({
+           format: 'MMM-DD-YYYY HH:mm:ss'
+       }),
+   */
+const logger = winston.createLogger({
+    level: 'debug',
+    format: combine(
+        timestamp({format : 'HH:mm DD-MM-YYYY'}),
+        myFormat
+    ),
+    transports: [
+        new winston.transports.File({ filename: './logs/fundonotes.log' })
+    ],
+});
+
+export default logger
