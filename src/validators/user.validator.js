609de63963311612d408448622565dc54aa8d7e0
Mazhar-4321 <syedmazharali742@gmail.com>
2023-02-04 07:49:14 +0530

    [Mazhar]ADDED: Password,Login And Email Validator

--- src/validators/user.validator.js
+++ src/validators/user.validator.js
@@ -7,19 +7,81 @@ export const newUserValidator = (req, res, next) => {
     lastName: Joi.string().pattern(new RegExp('([A-Z][a-zA-Z]{2,})')).required(),
     email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
     password: joiPassword
-    .string()
-    .minOfSpecialCharacters(1)
-    .minOfLowercase(4)
-    .minOfUppercase(1)
-    .minOfNumeric(2)
-    .noWhiteSpaces()
-    .required(),
+      .string()
+      .minOfSpecialCharacters(1)
+      .minOfLowercase(4)
+      .minOfUppercase(1)
+      .minOfNumeric(2)
+      .noWhiteSpaces()
+      .required(),
   });
   const { error, value } = schema.validate(req.body);
   if (error) {
-    next(error);
+    res.status(400).json({
+      code: 400,
+      error: error.message.split(":")[1],
+    });
   } else {
-    req.validatedBody = value;
+    next();
+  }
+};
+
+export const passwordValidator = (req, res, next) => {
+  const schema = Joi.object({
+    password: joiPassword
+      .string()
+      .minOfSpecialCharacters(1)
+      .minOfLowercase(4)
+      .minOfUppercase(1)
+      .minOfNumeric(2)
+      .noWhiteSpaces()
+      .required(),
+  });
+  const { error, value } = schema.validate(req.body);
+  if (error) {
+    res.status(400).json({
+      code: 400,
+      error: error.message.split(":")[1],
+    });
+  } else {
+    next();
+  }
+};
+
+export const loginValidator = (req, res, next) => {
+  const schema = Joi.object({
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+    password: joiPassword
+      .string()
+      .minOfSpecialCharacters(1)
+      .minOfLowercase(4)
+      .minOfUppercase(1)
+      .minOfNumeric(2)
+      .noWhiteSpaces()
+      .required(),
+  });
+  const { error, value } = schema.validate(req.body);
+  if (error) {
+    res.status(400).json({
+      code: 400,
+      error: error.message.split(":")[1],
+    });
+  } else {
+    next();
+  }
+};
+
+export const emailValidator = (req, res, next) => {
+  const schema = Joi.object({
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required()
+  });
+  const { error, value } = schema.validate(req.body);
+  if (error) {
+    res.status(400).json({
+      code: 400,
+      error: error.message.split(":")[1],
+    });
+  } else {
     next();
   }
 };
