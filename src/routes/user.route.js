473ba8b078c08030beec8e52fec11c90c294639b
Mazhar-4321 <syedmazharali742@gmail.com>
2023-02-04 07:52:53 +0530

    [Mazhar]ADDED: Validators To All User Routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,16 +1,16 @@
 import express from 'express';
 import * as userController from '../controllers/user.controller';
-import { newUserValidator } from '../validators/user.validator';
+import { newUserValidator,passwordValidator,loginValidator, emailValidator } from '../validators/user.validator';
 import { userAuthForReset } from '../middlewares/auth.middleware'
 
 const router = express.Router();
 
 router.post('/', newUserValidator, userController.registerUser);
 
-router.post('/login', userController.signInUser);
+router.post('/login',loginValidator, userController.signInUser);
 
-router.post('/reset/:id',userAuthForReset, userController.resetPassword)
+router.put('/reset/:id', passwordValidator, userAuthForReset, userController.resetPassword)
 
-router.post('/forget',userController.forgetPassword)
+router.put('/forget',emailValidator, userController.forgetPassword)
 
 export default router;
