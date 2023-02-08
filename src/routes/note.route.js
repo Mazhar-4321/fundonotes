73e005e498550514c91c3a76dcec61161ebfd1f2
Mazhar-4321 <syedmazharali742@gmail.com>
2023-02-08 15:48:50 +0530

    [Mazhar]ADDED: Redis Middleware To Note Route

--- src/routes/note.route.js
+++ src/routes/note.route.js
@@ -2,29 +2,29 @@ import express from 'express'
 import * as NoteController from '../controllers/note.controller'
 import { newNoteValidator } from '../validators/note.validator'
 import { userAuth } from '../middlewares/auth.middleware'
-
+import { checkRedisForNotes } from '../middlewares/redis.middleware'
 const router = express.Router()
 
-router.post('/', newNoteValidator,userAuth, NoteController.createNote)
+router.post('/', newNoteValidator, userAuth, NoteController.createNote)
 
-router.put('/archive/:id',userAuth,NoteController.updateNoteArchiveStatus)
+router.put('/archive/:id', userAuth, NoteController.updateNoteArchiveStatus)
 
-router.put('/trash/:id',userAuth,NoteController.updateTrashNote)
+router.put('/trash/:id', userAuth, NoteController.updateTrashNote)
 
-router.put('/:id',newNoteValidator,userAuth, NoteController.updateNote)
+router.put('/:id', newNoteValidator, userAuth, NoteController.updateNote)
 
-router.get('/trash/:id',userAuth,NoteController.findTrashNote)
+router.get('/trash/:id', userAuth, NoteController.findTrashNote)
 
-router.get('/archive/:id',userAuth,NoteController.findArchivedNote)
+router.get('/archive/:id', userAuth, NoteController.findArchivedNote)
 
-router.get('/trash',userAuth,NoteController.findAllTrashNotes)
+router.get('/trash', userAuth, NoteController.findAllTrashNotes)
 
-router.get('/:id',userAuth,NoteController.findNote)
+router.get('/:id', userAuth, NoteController.findNote)
 
-router.get('/',userAuth,NoteController.findAllNotes)
+router.get('/', userAuth, checkRedisForNotes, NoteController.findAllNotes)
 
-router.delete('/:id',userAuth,NoteController.deleteNote)
+router.delete('/:id', userAuth, NoteController.deleteNote)
 
-router.delete('/trash/:id',userAuth,NoteController.deleteTrashNote)
- 
+router.delete('/trash/:id', userAuth, NoteController.deleteTrashNote)
+
 export default router
